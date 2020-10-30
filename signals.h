223a4d0ec1880f57430b2efff2c586b603fd0d1e
AlexKrudu <KrudAlex@yandex.ru>
2020-10-31 00:18:59 +0300

    Seems done

--- signals.h
+++ signals.h
@@ -1,27 +1,145 @@
 #pragma once
+
 #include <functional>
+#include "intrusive_list.h"
 
-namespace signals
-{
+namespace signals {
 
-template <typename T>
-struct signal;
+    template<typename T>
+    struct signal;
 
-template <typename... Args>
-struct signal<void (Args...)>
-{
-    struct connection;
+    template<typename... Args>
+    struct signal<void(Args...)> {
+    public:
+        using slot_t = std::function<void(Args...)>;
+        struct connection;
+        struct connection_tag;
+        using connections_t = intrusive::list<connection, connection_tag>;
 
-    signal();
+        struct iteration_token {
+        private:
+            typename connections_t::const_iterator current;
+            iteration_token *next = nullptr;
+            signal const *sig = nullptr;
+            friend struct signal;
+        public:
+            iteration_token(const iteration_token &) = delete;
 
-    signal(signal const&) = delete;
-    signal& operator=(signal const&) = delete;
+            iteration_token &operator=(const iteration_token &) = delete;
 
-    ~signal();
+            explicit iteration_token(signal const *other) : sig(other) {
+                current = sig->connections.begin();
+                next = sig->top_token;// todo какого фига(((
+                sig->top_token = this;
+            }
 
-    connection connect(std::function<void (Args...)> slot) noexcept;
+            ~iteration_token() {
+                if (sig != nullptr) {
+                    sig->top_token = next;
+                }
+            }
 
-    void operator()(Args...) const;
-};
+        };
+
+        struct connection : intrusive::list_element<connection_tag> {
+        public:
+            connection() = default;
+
+            connection(connection &&other) noexcept: slot(std::move(other.slot)), sig(other.sig) {
+                if (sig != nullptr) {
+                    sig->connections.insert(sig->connections.as_iterator(other), *this);
+                    other.unlink();
+                    for (iteration_token *tok = sig->top_token; tok; tok = tok->next) {
+                        if (tok->current != sig->connections.end() && &*tok->current == &other) {
+                            tok->current = sig->connections.as_iterator(*this);
+                        }
+                    }
+                }
+            }
+
+            connection &operator=(connection &&other) noexcept {
+                if (this != &other) {
+                    disconnect();
+                    slot = std::move(other.slot);
+                    sig = other.sig;
+                    if (sig != nullptr) {
+                        sig->connections.insert(sig->connections.as_iterator(other), *this);
+                        other.unlink();
+                        for (iteration_token *tok = sig->top_token; tok; tok = tok->next) {
+                            if (tok->current != sig->connections.end() && &*tok->current == &other) {
+                                tok->current = sig->connections.as_iterator(*this);
+                            }
+                        }
+                    }
+                }
+                return *this;
+            };
+
+            explicit connection(signal *sig, slot_t slot) : slot(std::move(slot)), sig(sig) {
+                sig->connections.push_front(*this);
+            };
+
+            ~connection() {
+                disconnect();
+            }
+
+            void disconnect() {
+                if (this->is_linked() && sig) {
+                    this->unlink();
+                    slot = {};
+                    for (iteration_token *tok = sig->top_token; tok; tok = tok->next) {
+                        if (&*tok->current == this) {
+                            ++tok->current;
+                        }
+                    }
+                    sig = nullptr;
+                }
+            }
+
+        private:
+            slot_t slot;
+            signal *sig = nullptr;
+            friend struct signal;
+        };
+
+
+        signal() = default;
+
+        signal(signal const &) = delete;
+
+        signal &operator=(signal const &) = delete;
+
+        ~signal() {
+            for (iteration_token *tok = top_token; tok; tok = tok->next) {
+                tok->sig = nullptr;
+            }
+            while (!connections.empty()) {
+                auto cur = connections.begin();
+                cur->unlink();
+                cur->slot = {};
+                cur->sig = nullptr;
+            }
+        };
+
+        connection connect(slot_t slot) noexcept {
+            return connection(this, slot);
+        };
+
+        void operator()(Args... args) const {
+            iteration_token tok(this);
+            while (tok.current != connections.end()) {
+                auto copy = tok.current;
+                tok.current++;
+                copy->slot(args...);
+                if (tok.sig == nullptr) {
+                    return;
+                }
+            }
+        }
+
+    private:
+        connections_t connections;
+        mutable iteration_token *top_token = nullptr;
+    };
 
 }
